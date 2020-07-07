452565e383ab49fb15c3aa3e98def258de982801
stormymayday <stormymayday7@gmail.com>
2020-07-07 12:38:36 -0600

    Implemented form submission.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
   }
   return (
     <>
-      <Navbar  toggleSidebar={toggleSidebar}/>
+      <Navbar toggleSidebar={toggleSidebar}/>
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
       {children}
       <Footer/>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,11 +7,23 @@ const contact = () => {
 			<section className="contact-page">
 				<article className="contact-form">
 					<h3>get in touch</h3>
-					<form>
+					<form action="https://formspree.io/xleppovg" method="POST">
 						<div className="form-group">
-							<input type="text" placeholder="name" className="form-control" />
-							<input type="email" placeholder="email" className="form-control" />
-							<textarea name="message" rows="5" placeholder="message" className="form-control" />
+							<input type="text" name="name" maxlength="50" placeholder="name" className="form-control" />
+							<input
+								type="email"
+								name="email"
+								maxlength="100"
+								placeholder="email"
+								className="form-control"
+							/>
+							<textarea
+								name="message"
+								rows="5"
+								placeholder="message"
+								maxlength="500"
+								className="form-control"
+							/>
 						</div>
 						<button type="submit" className="submit-btn btn">
 							submit
